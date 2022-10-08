efa0a8e99b7ac069f520610211213c345dd076da
Violetta Sim <violetta.sim@yahoo.com>
2022-10-08 15:04:11 +0300

    #3 add search repos with security labels

---
+++ search_repos.py
@@ -0,0 +1,29 @@
+import github
+from secrets import MY_TOKEN
+
+from datetime import datetime
+import pause
+
+
+def is_s_label(label_name):
+    return 'security' in label_name.lower()
+
+
+g = github.Github(login_or_token=MY_TOKEN, per_page=100)
+repos = g.get_repos(since=1, visibility='public')
+
+for repo in repos:
+    if g.rate_limiting[0] < 10:
+        reset_time = datetime.fromtimestamp(g.rate_limiting_resettime)
+        print(f"waiting to reset at {reset_time}")
+        pause.until(reset_time)
+
+    try:
+        for label in repo.get_labels():
+            if is_s_label(label.name):
+                with open("security_label_repos.txt", 'a') as f:
+                    n = f.write(f'{repo.full_name}\n')
+        print(f'left: {g.rate_limiting[0]}      checked: {repo.id} — {repo.full_name}')
+
+    except Exception as e:
+        print(e)
